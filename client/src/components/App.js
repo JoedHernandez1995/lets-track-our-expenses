d4ed9570af14e878671a9f3cd2695badf96cce24
Jose Hernandez <josehernandez@Joses-MacBook-Pro.local>
2019-04-20 15:58:00 -0600

    creating login and registration screens

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Let's Track Our Expenses</h1>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
