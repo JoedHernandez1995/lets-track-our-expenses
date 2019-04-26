22c99897598382034738bf8d0877b7ffac00fefb
Jose Hernandez <josehernandez@Joses-MacBook-Pro.local>
2019-04-26 00:35:37 -0600

    add all expenses and show remaining income

--- client/src/components/Expenses/ExpenseList.js
+++ client/src/components/Expenses/ExpenseList.js
@@ -11,34 +11,37 @@ class ExpenseList extends Component {
 	constructor(){
 		super();
 		this.state = {
-			expenses: []
+			expenses: [],
+			remainingIncome: 0
 		}
 	}
-
 
 	componentDidMount(){
 		this.getAllExpensesFromServer();
 	}
 
 	getAllExpensesFromServer(){
+		var totalExpenses = 0;
+		var totalIncome = 0;
 		var c = this;
 		var apiURL = "http://localhost:5000/getAllExpenses";
 		axios.get(apiURL)
 	   	.then(function (response) {
+	   		console.log(response);
 	   		c.setState({expenses: response.data});
+	   		response.data.map(expense => totalExpenses += expense.cost);
+	   		c.setState({remainingIncome: totalIncome - totalExpenses});
 	   	})
 	   	.catch(function (error) {
 	     	console.log(error);
 	   	});
 	}
 
-
-
 	render() {
-		const expenses = this.getAllExpensesFromServer();
 		return (
 			<div>
 				<h1>Expenses</h1>
+				<h5>Remaining Income: </h5> {this.state.remainingIncome}
 				{this.state.expenses.map(expense => {
 					return <li>{expense.location}</li>
 				})}
