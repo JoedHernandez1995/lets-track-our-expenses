421189089e20edaff97ab396918a2ad6fc39f732
Jose Hernandez <josehernandez@Joses-MacBook-Pro.local>
2019-04-27 17:32:39 -0600

    associate user to incomes

--- models/income.js
+++ models/income.js
@@ -3,9 +3,11 @@ module.exports = (sequelize, DataTypes) => {
   const Income = sequelize.define('Income', {
     amount: DataTypes.FLOAT,
     date: DataTypes.DATE,
-    label: DataTypes.STRING
+    label: DataTypes.STRING,
+    UserId: DataTypes.INTEGER
   }, {});
   Income.associate = function(models) {
+  	Income.belongsTo(model.User);
     // associations can be defined here
   };
   return Income;
