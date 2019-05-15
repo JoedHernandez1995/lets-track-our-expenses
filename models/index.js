a4f8aecce8034249b825fc6ad5f8798e56de3a7d
Jose Hernandez <josehernandez@Joses-MacBook-Pro.local>
2019-05-15 01:30:08 -0600

    sequelize configuration

--- models/index.js
+++ models/index.js
@@ -8,6 +8,16 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
+
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres'
+  })
+}
+
+
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
